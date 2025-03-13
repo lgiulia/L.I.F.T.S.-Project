cfe476dafdd7366436173531e63c07d6103e8030
Elibott <bottura092@gmail.com>
2025-03-13 11:17:50 +0100

    minor fixes
    updated AGV_TelemetryData, still in development.

---
+++ config/mqtt_conf_params.py
@@ -0,0 +1,12 @@
+
+class MqttConfigurationParameters(object):
+    BROKER_ADDRESS =  "153.183.5.5"
+    BROKER_PORT = 7883
+    MQTT_USERNAME = "305609@studenti.unimore.it"
+    MQTT_PASSWORD = "osklduvrzblhnjdd"
+    MQTT_BASIC_TOPIC = "/iot/user/{0}".format(MQTT_USERNAME)
+#    VEHICLE_TOPIC = "vehicle"
+#    VEHICLE_TELEMETRY_TOPIC = "telemetry"
+#    VEHICLE_INFO_TOPIC = "info"
+
+#dobbiamo capire cosa metterci dentro strada facendo
--- model/AGV_TelemetryData.py
+++ model/AGV_TelemetryData.py
@@ -15,10 +15,14 @@ class AGVTelemetryData:
         return json.dumps(self, default=lambda o: o.__dict__)
 
     def update_measurements(self): #per emulare la generazione di nuovi valori in modo random
-        random_x = 0.0 + random.uniform(0.0, 50.0)  #si muove dalla posizione 0.0 alla posizione 50.0
+        random_x = 0.0 + random.uniform(0.0, 50.0)  #si muove dalla posizione 0.0 alla posizione 50.0, troppo veloce? considera coordx max =14, coordy max=16
         random_y = 0.0 + random.uniform(0.0, 50.0)
 
-        self.BatteryLevel -= random.uniform(0.0, 100.0) # diminuisce la batteria
-        # self.ON_OFF =
+        if self.BatteryLevel > 0 and self.ON_OFF == "ON":
+            self.BatteryLevel -= random.uniform(0.0, 2.0) # diminuisce la batteria gradualmente, se la batteria non è esaurita
+        else:
+            self.ON_OFF = "OFF" #la batteria è esaurita e l'agv si spegne
+
+        # self.ON_OFF = # bisognerebbe creare una funzione che controlla lo stato dell'agv e dice all'agv se essere acceso/spento/arrestato x motivi di sicurezza
         self.MissionCoordinates = Coordinates(random_x, random_y)
         self.timestamp = int(time.time())
