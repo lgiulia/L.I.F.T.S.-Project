5f6b0136cab95c5a39ecc574ee6d5697cf864b4a
Giulia <giulialui02@gmail.com>
2025-04-14 12:34:31 +0200

    updated data_manager.py

--- persistence/data_manager.py
+++ persistence/data_manager.py
@@ -10,13 +10,13 @@ class DataManager:
         if isinstance(newagv, AGVDescriptor):   # verifica se newDevice è un'istanza della classe AGVDescriptor
             self.AGV_dictionary[newagv.id] = newagv
         else:
-            raise TypeError("Error adding new AGV! Only AVGDescriptor are allowed!")
+            raise TypeError("Error adding new AGV! Only AGVDescriptor are allowed!")
 
     def update_agv(self, updatedagv):
         if isinstance(updatedagv, AGVDescriptor):
             self.AGV_dictionary[updatedagv.id] = updatedagv
         else:
-            raise TypeError("Error updating the AGV! Only AVGDescriptor are allowed!")
+            raise TypeError("Error updating the AGV! Only AGVDescriptor are allowed!")
 
     def remove_agv(self, agvID):
         if agvID in self.AGV_dictionary.keys():
