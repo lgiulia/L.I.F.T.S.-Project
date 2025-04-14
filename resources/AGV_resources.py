87c41078b7e8259c29da7a968094b6550e4ae726
Giulia <giulialui02@gmail.com>
2025-04-14 09:23:50 +0200

    updated AGV_resources

--- resources/AGV_resources.py
+++ resources/AGV_resources.py
@@ -1,3 +1,15 @@
 from json import JSONDecodeError
 from flask import request, Response
 from flask_restful import Resource, reqparse
+from model.AGV_descriptor import AGVDescriptor
+
+class AGVResource(Resource):
+
+    def __init__(self, **kwargs):   # **kwargs accetta argomenti a parole chiave arbitrarie
+        self.dataManager = kwargs['data_manager']  # assegna l'oggetto data_manager all'attributo self.dataManager dell'istanza
+
+    def get(self):  # metodo per gestire le richieste HTTP GET
+        device_list = []
+        for device in self.dataManager.device_dictionary.values():
+            device_list.append(device.__dict__)
+        return device_list, 200   # 200: codice HTTP OK
