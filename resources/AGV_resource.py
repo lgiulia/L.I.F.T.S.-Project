30b8054427566d9a31119bf821b01c5c3e106bf2
Giulia <giulialui02@gmail.com>
2025-04-15 09:23:36 +0200

    added put method

--- resources/AGV_resource.py
+++ resources/AGV_resource.py
@@ -16,3 +16,28 @@ class AGVResource(Resource):
             return self.dataManager.AGV_dictionary[agv_id].__dict__, 200
         else:
             return {'error': "AGV Not Found"}, 404
+
+    def put(self, agv_id):
+        if agv_id not in self.dataManager.AGV_dictionary:
+            return {'error': f"AGV with id '{agv_id}' Not Found"}, 404
+
+        try:
+            data = request.get_json(force=True)
+            existing_agv = self.dataManager.AGV_dictionary[agv_id]
+
+            # Crea un nuovo AGVDescriptor con i dati aggiornati (mantenendo i valori esistenti se non forniti)
+            updated_agv = AGVDescriptor(
+                id=agv_id,  # L'ID viene preso dall'URL
+                manufacturer=data.get('manufacturer', existing_agv.manufacturer),
+                model=data.get('model', existing_agv.model)
+            )
+
+            # Aggiorna l'AGV nel DataManager
+            self.dataManager.update_agv(updated_agv)
+
+            return '', 204  # Aggiornamento riuscito, nessun body
+
+        except JSONDecodeError:
+            return {'error': "Invalid JSON! Check the request"}, 400
+        except Exception as e:
+            return {'error': f"Internal Server Error: {str(e)}"}, 500
