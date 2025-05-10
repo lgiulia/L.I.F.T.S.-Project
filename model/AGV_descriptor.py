1e8281a0506f0b127ce9e6b898f5a78987c6986b
Giulia <giulialui02@gmail.com>
2025-05-10 17:13:07 +0200

    updated AGV_descriptor
    added senml

--- model/AGV_descriptor.py
+++ model/AGV_descriptor.py
@@ -1,14 +1,15 @@
 import json
-from wsgiref.simple_server import software_version
+#from wsgiref.simple_server import software_ver
 
 
 class AGVDescriptor:
 
-    def __init__(self, id, manufacturer, model, software_ver):
+    def __init__(self, id, manufacturer, model, software_ver, senml=None):
         self.id = id
         self.manufacturer = manufacturer
         self.model = model
         self.software_ver = software_ver
+        self.senml = senml
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__)
